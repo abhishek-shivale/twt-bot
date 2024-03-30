656a6b74b4bd74c6f2625b6b9ece2d21238ddd46
CommitTODev <abhishekshivale45@gmail.com>
2024-03-30 19:15:48 +0530

    fixed error

--- index.js
+++ index.js
@@ -12,38 +12,44 @@ const client = new TwitterApi({
   accessSecret: process.env.ACCESS_TOKEN_SECRET,
 })
 
-console.log(process.env.APP_KEY);
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API);
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API); 
 
-export const twitterClient = client.readWrite
+export const twitterClient = client.readWrite;
 
+async function NewTweet() {
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const AI_PROMPT = prompt[Math.floor(Math.random() * 10) + 1];
+    const res = await model.generateContent(
+      AI_PROMPT +
+        "write in simple and short english below 250 characters. Twitter currently allows a maximum of 250 characters per tweet. This includes all text, spaces, punctuation, and mentions (@ usernames) but excludes media links shortened by Twitter (using t.co)"
+    );
+    const content = res.response;
+    const textContent = content.text().replace(/\*/g, "");
 
-async function NewTweeT() {
-   try {
-     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-     const AI_PROMPT = prompt[Math.floor(Math.random() * 10) + 1];
-     const res = await model.generateContent(
-       AI_PROMPT +
-         "write in simple and short english below 250 character Twitter currently allows a maximum of 250 characters per tweet. This includes all text, spaces, punctuation, and mentions (@ usernames) but excludes media links shortened by Twitter (using t.co)"
-     );
-     const content = res.response;
-     const textCotent = content.text().replace(/\*/g, "");
-     if (textCotent.length > 280) {
-       console.error("Tweet text is too long:", textCotent.length);
-     } else {
-       const twitterResponse = await twitterClient.v2.tweet({
-         text: textCotent,
-       });
-     }
-     console.log("tweet is done!!!");
-} catch (err) {
-    console.error(err);
-    console.log("Found some error!!")
-   }
+    if (textContent.length > 280) {
+      console.error("Tweet text is too long:", textContent.length);
+       NewTweet(); 
+      return; 
+    }
+
+    const twitterResponse = await twitterClient.v2.tweet({
+      text: textContent,
+    });
+    console.log("Tweet posted successfully:", twitterResponse);
+  } catch (err) {
+    console.error("Error occurred:", err);
+    console.log("Retrying tweet generation in 5 seconds...");
     
+    await new Promise((resolve) => setTimeout(resolve, 5000)); 
+    NewTweet(); 
+  } finally {
+    console.log("Tweet generation complete (success or error)");
+  }
 }
-cron.schedule("*/30 * * * *",()=>{
-    NewTweeT(),
-    console.log("corn is working");
+
+cron.schedule("*/45 * * * *", () => {
+  NewTweet();
+  console.log("Cron job running");
 });
 
